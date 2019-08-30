461a0097cf41f846214b3567d687a060635b78b8
jamessssszgc <zhangg18@outlook.com>
2019-08-30 15:46:30 -0400

    sd

---
+++ src/components/projects.jsx
@@ -0,0 +1,76 @@
+import React, { Component } from 'react'
+
+export default class Projects extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-experience" data-section="projects">
+          <div className="colorlib-narrow-content">
+            <div className="row">
+              <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                <span className="heading-meta">Projects</span>
+                <h2 className="colorlib-heading animate-box">Recent Projects</h2>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+                <div className="timeline-centered">
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-3">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>AUTOMATIC REIGION FILLING & REMOVAL <span>2018</span></h2>
+                        <p>	Performed object recognition training using <b>mask-RCNN</b> to detect the unfavored objects in the given image. Implemented and improved the <b>CBIR</b> algorithm to automatically erase the unfavored object and to perform image inpainting to fill in small image gaps</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInTop">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-4">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>CONDO RATING WEBSITE<span>2018</span></h2>
+                        <p>Inspired by the RateMyProfessor website, I wanted to build a website that can help people evaluate the condo or apartment they plan to move in from a insider's perspective. Current residents can register and login to the website and put down their opinions and rating regarding the building, while the perspective tenants or buyers can search for the building they are interested in using GoogleMap API. As a team lead of 4, we designed the <b>RESTful</b> website with a responsive front-end page in <b>Bootstrap</b>, <b>JQuery</b>, back-end in <b>Nodejs</b> and <b>Mongodb</b></p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-5">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>EXT2 File System <span>2018</span></h2>
+                        <p>Implemented the EXT2 file system and wrote tools to modify the EXT2-format <b>virtual disk</b>. Simulated operations of a 2-level <b>Page Table</b> and <b>Page Replacement algorithm</b></p> 
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-1">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Partially Observed Image Completion <span>2018</span></h2>
+                        <p>Implemented a probabilistic model using EM algorithm which can apply to the image completion of partially observed handwritten digits. Achieved significantly higher average log likelihood with the EM model than the average log likelihood for with the supervised learning model.</p> 
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry begin animate-box" data-animate-effect="fadeInBottom">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-none">
+                      </div>
+                    </div>
+                  </article>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    )
+  }
+}
---
+++ src/components/timeline.jsx
@@ -0,0 +1,68 @@
+import React, { Component } from 'react'
+
+export default class Timeline extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-experience" data-section="timeline">
+          <div className="colorlib-narrow-content">
+            <div className="row">
+              <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                <span className="heading-meta">highlights</span>
+                <h2 className="colorlib-heading animate-box">Timeline</h2>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+                <div className="timeline-centered">
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-3">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Software Engineer Intern at EchoWorx Corp. <span>2018-2019</span></h2>
+                        <p>Designed class interfaces with Singleton and Factory Design Pattern and implemented automation test suite framework for new and existing features. Developed an automated database migration test in MySQL to detect and fix data inconsistency. Maintained and configured Jenkins and web services such as Apache Tomcat, Apache James on Linux to support the R&D and QA team. Participated in an Agile development (Scrum) team, improved the user interface using Java Spring framework and merged mobile web pages into Responsive Designed Front End Pages in jsp.</p>
+                      </div>
+                    </div>
+                  </article>
+                  {/*
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInTop">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-4">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Computer Vision Intern at Huron Digital Pathology<span>2019</span></h2>
+                        <p>Investigate several image feature extraction techniques and applied to the Whole Slide Image to evaluate their performance on histological images.
+                        Developed a secured web application in NodeJS and React that helps the team communicating and sharing data with researchers in other institutions</p>
+                      </div>
+                    </div>
+                  </article>
+                  */}
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-5">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Research Assistant at University of Toronto <span>2017-2018</span></h2>
+                        <p>Programmed a secured web application in Nodejs and React for the purpose of collecting audio recordings and response time from participants. Improved the previous audio processing model and minimized the audio processing latency to below 20ms. Designed a MySQL database to store the data collected in the experiment and to avoid web vulnerabilities</p> 
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry begin animate-box" data-animate-effect="fadeInBottom">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-none">
+                      </div>
+                    </div>
+                  </article>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    )
+  }
+}
